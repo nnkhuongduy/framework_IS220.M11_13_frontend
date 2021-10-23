4e6ecaba23d10084375e1724df25e0ca1503203f
KhuongDuy <60511087+nnkhuongduy@users.noreply.github.com>
2021-10-23 21:53:36 +0700

    setting up the application (#1)

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { theme } from './theme';
+import { store } from './slices/store';
+
+import localizedFormat from 'dayjs/plugin/localizedFormat';
+import utc from 'dayjs/plugin/utc';
+import dayjs from 'dayjs';
+
+dayjs.extend(localizedFormat);
+dayjs.extend(utc);
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
---
+++ src/styled.d.ts
@@ -0,0 +1,37 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    pageMargin: {
+      xl: number;
+      lg: number;
+      md: number;
+      sm: number;
+      xs: number;
+    };
+
+    breakpoints: {
+      lg: number;
+      md: number;
+      sm: number;
+      xs: number;
+    };
+
+    colors: {
+      primary: {
+        light: string;
+        main: string;
+        dark: string;
+      };
+      secondary: {
+        light: string;
+        main: string;
+        dark: string;
+      };
+      backgrounds: {
+        dark: string;
+        light: string;
+      };
+    };
+  }
+}
